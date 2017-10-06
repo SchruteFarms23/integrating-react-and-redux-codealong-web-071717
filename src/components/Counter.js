e6b54bd4f4bcbe591fe43ed59110e36a858de0b3
SchruteFarms23 <romanmullady@gmail.com>
2017-10-06 12:56:45 -0400

    Done.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-export default (props) => {
-  return (
-    <div>
-      Counter Component
-    </div>
-  )
-};
+ export default (props) => {
+
+   const handleOnClick = () => {
+     props.store.dispatch({ type: 'INCREASE_COUNT' });
+   }
+
+   const decrease = () =>{
+     props.store.dispatch({ type: 'DECREASE_COUNT' });
+   }
+
+   return (
+     <div>
+       <button onClick={handleOnClick}>+</button>
+       <button onClick={decrease} >-</button>
+       <div>{props.store.getState().count}</div>
+     </div>
+   )
+ };
